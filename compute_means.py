1313eda759e97066926ce1049fafb68bd16ea395
megaelius <abad.elias00@gmail.com>
2022-01-16 00:03:48 +0100

    reorganize

--- compute_means.py
+++ compute_means.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 if __name__ == '__main__':
-    df = pd.read_csv('experiments.csv', sep = ';')
+    df = pd.read_csv('results/experiments.csv', sep = ';')
     exp_names = pd.unique(df['Name'])
     for name in exp_names:
         for var in ['AP','acc']:
             mean_overall_acc = np.mean([float(v) for v in df[(df['Name'] == name) & (df['run'] != 'mean')][var]])
             df.loc[(df['Name'] == name) & (df['run'] == 'mean'),var] = mean_overall_acc
-    df.to_csv('experiments.csv', index = False, sep = ';')
+    df.to_csv('results/experiments.csv', index = False, sep = ';')
